ea5f91cdb2a5b84a41740d7eeb412f274f9c1089
And5reas <And5reas.porcel@hotmail.com>
2026-03-21 22:16:36 -0300

    remove: remote the entire project

--- src/domain/repositories/ILeadRepository.js
+++
@@ -1,6 +0,0 @@
-export class ILeadRepository {
-  async findById(id) { throw new Error('Not implemented'); }
-  async findAllByCompany(companyId) { throw new Error('Not implemented'); }
-  async getLeadsByStage(stageId) { throw new Error('Not implemented'); }
-  async getStagnantCandidates(companyId, batchSize = 1000) { throw new Error('Not implemented'); }
-}
