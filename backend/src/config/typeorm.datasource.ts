dc4d47a3075568bea2cb6dd663d607afbccbafae
And5reas <And5reas.porcel@hotmail.com>
2026-05-26 08:22:36 -0300

    update: typeorm.datasource to git deploy

--- backend/src/config/typeorm.datasource.ts
+++ backend/src/config/typeorm.datasource.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.NODE_ENV === 'production' ? 'postgres-svc.infra-banco.svc.cluster.local' : 'localhost',
   port: parseInt('5432', 10),
-  database: 'sales_weakness',
-  username: 'salesweakness',
-  password: 'salesweakness',
+  database: process.env.NODE_ENV === 'production' ? 'infra_banco' : 'sales_weakness',
+  username: process.env.NODE_ENV === 'production' ? 'user_crm_leads' : 'salesweakness',
+  password: process.env.NODE_ENV === 'production' ? 'SenhaCrm123!' : 'salesweakness',
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/database/migrations/*.ts'],
   subscribers: ['src/**/*.subscriber.ts'], 
